7ce04ecbab93c54e71eb157a4dee20d990391579
Baruch Shadrouz <baruchshad@gmail.com>
2020-12-26 21:04:18 -0500

    Initial commit

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import '../helpers/db_helper.dart';
+import '../models/place.dart';
+
+class GreatPlace with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items {
+    return [..._items];
+  }
+
+  void addPlace(String title, File image) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: image,
+      title: title,
+      location: null,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map((item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null))
+        .toList();
+    notifyListeners();
+  }
+}
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
+
+class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput({this.onSelectImage});
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+
+    final imageFile = await picker.getImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if (imageFile == null) return;
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await _storedImage.copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 200,
+          height: 200,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          alignment: Alignment.center,
+          child: _storedImage == null
+              ? Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                )
+              : Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            child: FlatButton.icon(
+          icon: Icon(Icons.camera),
+          label: Text('Take Picture'),
+          textColor: Theme.of(context).primaryColor,
+          height: 200,
+          onPressed: _takePicture,
+        ))
+      ],
+    );
+  }
+}
